877e12033bc4834686ca8a1ac6cf753771b9d48a
LeeJaeHoon <dlwogns3413@naver.com>
2021-12-09 18:37:36 +0900

    learn takeEvery and takeLatest

--- sagas/index.js
+++ sagas/index.js
@@ -1,16 +1,18 @@
-import { all, fork, take, put } from "redux-saga/effects";
+import { all, fork, call, put, takeLatest, delay } from "redux-saga/effects";
 import axios from "axios";
 
+//이부분은 제너레이터 아니다.
+// 서버 요청 부분
 function loginAPI(data) {
   return axios.post("/api/login", data);
 }
 
 function* login(action) {
   try {
-    const result = yield call(loginAPI, action.data); //call fork 차이 fork는 비동기 call은 동기
+    // const result = yield call(loginAPI, action.data); //call fork 차이 fork는 비동기 call은 동기
+    yield delay(1000);
     yield put({
       type: "LOG_IN_SUCCESS",
-      data: result.data,
     });
   } catch (err) {
     yield put({
@@ -27,10 +29,10 @@ function logoutAPI() {
 
 function* logout() {
   try {
-    const result = yield call(logoutAPI); //call fork 차이 fork는 비동기 call은 동기
+    //const result = yield call(logoutAPI); //call fork 차이 fork는 비동기 call은 동기
+    yield delay(1000);
     yield put({
       type: "LOG_OUT_SUCCESS",
-      data: result.data,
     });
   } catch (err) {
     yield put({
@@ -47,10 +49,10 @@ function addPostAPI(data) {
 
 function* addPost(action) {
   try {
-    const result = yield call(addPostAPI, action.data); //call fork 차이 fork는 비동기 call은 동기
+    //const result = yield call(addPostAPI, action.data); //call fork 차이 fork는 비동기 call은 동기
+    yield delay(1000);
     yield put({
       type: "ADD_POST_SUCCESS",
-      data: result.data,
     });
   } catch (err) {
     yield put({
@@ -62,20 +64,25 @@ function* addPost(action) {
 }
 
 function* watchLogin() {
-  yield take("LOG_IN_REQUEST", login); //take는 LOG_IN이라는 액션이 실행될때까지 기다리겠다 라는 뜻
+  //take는 LOG_IN_REQUES이라는 액션이 실행될때까지 기다리겠다 라는 뜻 (동기)
+  //LOG_IN_REQUES이라는 액션이 실행되면 두번째 argument가 실행된다.
+  //일회용 딱 한번만 실행된다.
+  //takeEvery는 비동기
+  yield takeLatest("LOG_IN_REQUEST", login);
 }
 
 function* watchLogOut() {
-  yield take("LOG_OUT_REQUEST", logout);
+  yield takeLatest("LOG_OUT_REQUEST", logout);
 }
 
 function* watchAddPost() {
-  yield take("ADD_POST_REQUEST", addPost);
+  // 마지막 액션만 처리해줌
+  yield takeLatest("ADD_POST_REQUEST", addPost);
 }
 
 export default function* rootSaga() {
   yield all([
-    //all은 배열안에 들어가 있는것을 동시에 실행시킨다.
+    //all은 배열안에 들어가 있는것을 동시에(한방에?) 실행시킨다.
     fork(watchLogin), //fork는 인자에 들어가있는 함수를 실행시킨다.
     fork(watchLogOut),
     fork(watchAddPost),
