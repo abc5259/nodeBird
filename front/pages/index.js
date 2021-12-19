397bdaa8dbeefb178345468cb1eeb7d854115387
LeeJaeHoon <dlwogns3413@naver.com>
2021-12-19 17:13:23 +0900

    feat: working ssr

--- front/pages/index.js
+++ front/pages/index.js
@@ -6,6 +6,8 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { LOAD_POST_REQUEST } from "../reducers/post";
 import { LOAD_MY_INFO_REQUEST } from "../reducers/user";
+import wrapper from "../store/configureStore";
+import { END } from "@redux-saga/core";
 const Home = () => {
   const dispatch = useDispatch();
   const { me } = useSelector(state => state.user);
@@ -18,15 +20,6 @@ const Home = () => {
       alert(retweetError);
     }
   }, [retweetError]);
-
-  useEffect(() => {
-    dispatch({
-      type: LOAD_MY_INFO_REQUEST,
-    });
-    dispatch({
-      type: LOAD_POST_REQUEST,
-    });
-  }, []);
 
   useEffect(() => {
     const onScroll = e => {
@@ -59,5 +52,24 @@ const Home = () => {
     </>
   );
 };
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  store =>
+    async ({ req }) => {
+      // const cookie = req ? req.headers.cookie : "";
+      // axios.defaults.headers.Cookie = "";
+      // if (req && cookie) {
+      //   axios.defaults.headers.Cookie = cookie;
+      // }
+      store.dispatch({
+        type: LOAD_MY_INFO_REQUEST,
+      });
+      store.dispatch({
+        type: LOAD_POST_REQUEST,
+      });
+      store.dispatch(END);
+      await store.sagaTask.toPromise();
+    }
+);
 
 export default Home;
