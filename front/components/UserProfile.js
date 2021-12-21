f7552c692af5ac1f9fe54238ddad79079c27f873
LeeJaeHoon <dlwogns3413@naver.com>
2021-12-22 00:21:51 +0900

    feat: Link clear

--- front/components/UserProfile.js
+++ front/components/UserProfile.js
@@ -3,6 +3,7 @@ import { Avatar, Button, Card } from "antd";
 import { useDispatch } from "react-redux";
 import { logoutRequestAction } from "../reducers/user";
 import { useSelector } from "react-redux";
+import Link from "next/link";
 
 const UserProfile = () => {
   const dispatch = useDispatch();
@@ -15,25 +16,43 @@ const UserProfile = () => {
     <>
       <Card
         actions={[
-          <div key="twit">
-            짹짹
-            <br />
-            {me.Posts.length}
-          </div>,
-          <div key="followings">
-            팔로잉
-            <br />
-            {me.Followings.length}
-          </div>,
-          <div key="followers">
-            팔로워
-            <br />
-            {me.Followers.length}
-          </div>,
+          <Link href={`/user/${me.id}`}>
+            <a>
+              <div key="twit">
+                짹짹
+                <br />
+                {me.Posts.length}
+              </div>
+            </a>
+          </Link>,
+          <Link href={`/profile`}>
+            <a>
+              <div key="followings">
+                팔로잉
+                <br />
+                {me.Followings.length}
+              </div>
+            </a>
+          </Link>,
+          <Link href={`/profile`}>
+            <a>
+              <div key="followers">
+                팔로워
+                <br />
+                {me.Followers.length}
+              </div>
+            </a>
+          </Link>,
         ]}
       >
         <Card.Meta
-          avatar={<Avatar>{me.nickname[0]}</Avatar>}
+          avatar={
+            <Link href={`/user/${me.id}`}>
+              <a>
+                <Avatar>{me.nickname[0]}</Avatar>
+              </a>
+            </Link>
+          }
           title={me.nickname}
         />
         <Button onClick={onLogOut} loading={logOutLoading}>
